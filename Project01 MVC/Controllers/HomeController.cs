688832fa93c85bf10493670ac90ce971d5b93671
bossyGad <bosygad179@gmail.com>
2024-09-08 23:51:57 +0300

    Add project files.

---
+++ Project01 MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project01_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ContentResult result = new ContentResult();
+            //result.Content = "Hello From Content Result";
+            return Content("Hello From Content Result");
+        }
+        public string AboutUs()
+        {
+            return "This is About Us Action";
+        }
+    }
+}
